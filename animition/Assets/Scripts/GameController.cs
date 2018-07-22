56a307d7d92ab7a5df26241ffde85ad34f624965
panghengtao <317596469@qq.com>
2018-07-22 22:44:30 +0800

    学习animation

---
+++ animition/Assets/Scripts/GameController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+    public GameObject bgGround;
+    public GameObject anotherBgGround;
+    public GameObject column;
+    public GameObject gameOverText;
+    private float bgLen;
+    public bool gameOver = false;
+
+    public float speed = -1.5f;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            
+            InitBgGround();
+        }
+		else if(instance != this)
+		{
+			Destroy(gameObject);
+		}
+    }
+
+    private void InitBgGround()
+    {
+        anotherBgGround = GameObject.Instantiate(bgGround).gameObject;
+        bgLen = bgGround.GetComponent<BoxCollider2D>().size.x;
+        Vector3 origin = bgGround.GetComponent<Transform>().position;
+        Vector3 offset = new Vector3(bgLen + origin.x, 0, 0);
+        anotherBgGround.GetComponent<Transform>().position = offset + origin;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        column.GetComponent<Rigidbody2D>().velocity = new Vector2(-1.5f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(gameOver == true && Input.GetMouseButtonDown(0))
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+    }
+
+    public void BirdDied()
+    {
+        gameOver = true;
+        gameOverText.SetActive(true);
+    }
+}
---
+++ animition/Assets/Scripts/ScrollingGround.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingGround : MonoBehaviour
+{
+
+
+    private Rigidbody2D rb2d;
+
+    private Transform trans;
+
+    private BoxCollider2D groundCollider;
+    private float bgLen;
+
+    void Start()
+    {
+        rb2d = this.GetComponent<Rigidbody2D>();
+        trans = GetComponent<Transform>();
+        groundCollider = GetComponent<BoxCollider2D>();
+
+        bgLen = groundCollider.size.x;
+
+        rb2d.velocity = new Vector2(GameController.instance.speed, 0);
+    }
+
+    void Update()
+    {
+        if (GameController.instance.gameOver == true)
+        {
+            rb2d.velocity = Vector2.zero;
+        }
+        else if (trans.position.x < -bgLen)
+        {
+            RpositionBackGround();
+        }
+    }
+
+    private void RpositionBackGround()
+    {
+        Vector3 offset = new Vector3(bgLen * 2f, 0, 0);
+        trans.position = trans.position + offset;
+    }
+}
